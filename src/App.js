1601ec95c94baadbd0c97ff281cff4b5d77e541d
Anton Loginov <you@example.com>
2018-11-25 21:43:12 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import Header from './Header';
+import {articles} from "./fixtures";
+import ArticleList from "./ArticleList";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <ArticleList articles={articles} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Article.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+
+export default class Article extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpen: false,
+    };
+
+    // this.handleOpen = this.handleOpen.bind(this);
+  }
+  handleOpen = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  };
+
+  getBody() {
+    if (!this.state.isOpen) return null;
+    const {article} = this.props;
+    return <section className="section">{article.text}</section>
+  }
+
+  render() {
+    const {article} = this.props;
+    const {isOpen} = this.state;
+    return (
+      <div>
+        <h3 className="title">{article.title}</h3>
+        <button onClick={this.handleOpen} className="btn">
+          {isOpen ? 'open': 'close'}
+        </button>
+        {this.getBody()}
+      </div>
+    )
+  }
+}
+
